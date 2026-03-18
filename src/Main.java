99250cf36360d2f30e7d3fb9079d752d47209636
22czahonyim-pixel <22c_zahonyim@nyirszikszi.hu>
2026-03-18 14:08:50 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-class Main{
-    public static void main(String[] args){
-        Dog kuyta = new Dog();
+class  Main{
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10,12);
 
-        kuyta.speak();
+        Circle circle = new Circle(5);
+
+        Triangle triangle = new Triangle(4,10);
+
+        System.out.println("A négyzet területe: " + rectangle.getArea());
+        System.out.println("A kör területe: " + circle.getArea());
+        System.out.println("A háromszög területe: " + triangle.getArea());
+
     }
 }
